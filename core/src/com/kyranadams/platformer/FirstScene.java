724950cf033d873f4e1a69e5b1b9f771a41207de
Kyran Adams <kyranstar@yahoo.com>
2016-10-04 19:06:31 -0700

    Fixed character movement, added placeholder art, loaded character position from tmx file

--- core/src/com/kyranadams/platformer/FirstScene.java
+++ core/src/com/kyranadams/platformer/FirstScene.java
@@ -4,27 +4,36 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 
 public class FirstScene extends GameScreen {
     private static final int SCROLL_SPEED = 5;
-    TiledMap tiledMap;
-    TiledMapRenderer tiledMapRenderer;
-    ControllableCharacter mainCharacter = new ControllableCharacter("badlogic.jpg");
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
+    private ControllableCharacter mainCharacter = new ControllableCharacter("george.png");
+    private static final int[] RENDERED_LAYERS = new int[]{0};
 
     public FirstScene(Game game) {
         super(game);
+        // load map
         tiledMap = new TmxMapLoader().load("sceneOneMap.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        // load character position
+        Vector2 characterStartPos = ((RectangleMapObject) tiledMap.getLayers().get("Entities").getObjects().get("George")).getRectangle().getPosition(new Vector2());
+        mainCharacter.setPosition(characterStartPos.x, characterStartPos.y);
+
+        // create stage
         stage = new Stage(new StretchViewport(SCREEN_WIDTH, SCREEN_HEIGHT));
-        mainCharacter.setPosition(0, 0);
         stage.addActor(mainCharacter);
     }
 
@@ -32,20 +41,32 @@ public class FirstScene extends GameScreen {
     protected void update(float delta) {
         stage.act(delta);
         if (Gdx.input.isTouched()) {
-            if (Gdx.input.getX() > SCREEN_WIDTH / 2) {
-                mainCharacter.moveBy(SCROLL_SPEED, 1);
+            // scale input to viewport
+            Vector3 touch = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+            stage.getCamera().unproject(touch);
+
+            // if right hand side, move right
+            if (touch.x > stage.getCamera().viewportWidth / 2) {
+                mainCharacter.moveBy(SCROLL_SPEED, 0);
             } else {
-                mainCharacter.moveBy(-SCROLL_SPEED, -1);
+                mainCharacter.moveBy(-SCROLL_SPEED, 0);
             }
 
         }
-        stage.getCamera().position.set(mainCharacter.getX(), mainCharacter.getY(), 0);
-        float camX = stage.getCamera().position.x;
-        float camY = stage.getCamera().position.y;
+        updateCamera(delta);
 
+    }
+
+    private void updateCamera(float delta) {
+        // camera follows player
+        stage.getCamera().position.set(mainCharacter.getX(), mainCharacter.getY(), 0);
+
+        // Camera borders
         Vector2 camMin = new Vector2(stage.getCamera().viewportWidth / 2, stage.getCamera().viewportHeight / 2);
         Vector2 camMax = new Vector2(tileMapWidthPixels() - stage.getCamera().viewportWidth / 2, tileMapHeightPixels() - stage.getCamera().viewportHeight / 2);
 
+        float camX = stage.getCamera().position.x;
+        float camY = stage.getCamera().position.y;
         //keep camera within borders
         camX = Math.min(camMax.x, Math.max(camX, camMin.x));
         camY = Math.min(camMax.y, Math.max(camY, camMin.y));
@@ -54,10 +75,11 @@ public class FirstScene extends GameScreen {
         stage.getCamera().update();
     }
 
-    private int tileMapWidthPixels(){
+    private int tileMapWidthPixels() {
         return tiledMap.getProperties().get("width", Integer.class) * tiledMap.getProperties().get("tilewidth", Integer.class);
     }
-    private int tileMapHeightPixels(){
+
+    private int tileMapHeightPixels() {
         return tiledMap.getProperties().get("height", Integer.class) * tiledMap.getProperties().get("tileheight", Integer.class);
     }
 
@@ -69,7 +91,7 @@ public class FirstScene extends GameScreen {
     @Override
     protected void renderScene(SpriteBatch batch) {
         tiledMapRenderer.setView((OrthographicCamera) stage.getCamera());
-        tiledMapRenderer.render();
+        tiledMapRenderer.render(RENDERED_LAYERS);
         stage.draw();
     }
 
